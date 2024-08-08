34027df07ef6808108ae758552fe7a12dcde2703
JisooPyo <audwl326@gmail.com>
2024-08-08 23:04:56 +0900

    update: PostService `@Transactional` 추가

--- src/main/java/cloudan/miniProject/service/PostServiceImpl.java
+++ src/main/java/cloudan/miniProject/service/PostServiceImpl.java
@@ -1,6 +1,7 @@
 package cloudan.miniProject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import cloudan.miniProject.common.constants.PostConstants;
 import cloudan.miniProject.common.dto.ApiResponseDto;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor        // 필요한 매개변수(final)로만 이루어진 생성자입니다.
+@Transactional                  // 트랜잭션
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
 
