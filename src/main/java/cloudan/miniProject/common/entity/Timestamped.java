505aa9f9e6ed77e38a09085d41af9dcafe4271d5
JisooPyo <audwl326@gmail.com>
2024-08-14 20:45:03 +0900

    [Feature] 게시글 삭제 기능 (#3)
    * feat: 게시글 삭제 기능 구현
    
    * chore: 필요없는 import문 제거
    
    * update: PostService `@Transactional` 추가
    
    * ci: Github Actions Secret 변수명 변경
    
    * ci: MySQL 설치 스크립트 변경
    
    * ci: MySQL 버전 변경 및 빌드 시 테스트 코드 빌드 제외
    
    * ci: MySQL, Java 설치 액션 변경
    
    * ci: build gradle - test code 실행 포함
    
    * ci: JDK 설치 액션 distribution 키워드 변경
    
    * ci: MySQL 설치 액션 root pw 설정
    
    * ci: MySQL 설치 액션 변경
    
    * docs: PR template - 아이콘 추가

--- src/main/java/cloudan/miniProject/common/entity/Timestamped.java
+++ src/main/java/cloudan/miniProject/common/entity/Timestamped.java
@@ -3,7 +3,6 @@ package cloudan.miniProject.common.entity;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
--- src/main/java/cloudan/miniProject/service/PostServiceImpl.java
+++ src/main/java/cloudan/miniProject/service/PostServiceImpl.java
@@ -1,12 +1,32 @@
 package cloudan.miniProject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import cloudan.miniProject.common.constants.PostConstants;
+import cloudan.miniProject.common.dto.ApiResponseDto;
+import cloudan.miniProject.common.exception.CustomErrorCode;
+import cloudan.miniProject.common.exception.CustomException;
+import cloudan.miniProject.model.Post;
 import cloudan.miniProject.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor        // 필요한 매개변수(final)로만 이루어진 생성자입니다.
+@Transactional                  // 트랜잭션
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
+
+    @Override
+    public ApiResponseDto deletePost(int id) {
+        findPost(id);
+        postRepository.deleteById(id);
+        return new ApiResponseDto(PostConstants.DELETE_SUCCESS);
+    }
+
+    private Post findPost(int id) {
+        return postRepository.findById(id).orElseThrow(
+            () -> new CustomException(CustomErrorCode.POST_NOT_FOUND)
+        );
+    }
 }
